de230f32b3f89b412e783098b261cfb53cf4c0e0
Zach Malter <zach@maltertech.com>
2024-03-06 00:35:54 -0500

    updates

---
+++ google-ads-custom-pixel.js
@@ -0,0 +1,46 @@
+// Google Tag
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag("js", new Date());
+
+gtag("config", "AW-XXXXXXXX", {"allow_enhanced_conversions": true});
+
+// GA4 add_to_cart
+analytics.subscribe("product_added_to_cart", (event) => {
+    gtag("event", "conversion", {
+        "send_to": "AW-XXXXXXXX/YYYYYYYYYYYYY",
+        "value": event.data.cartLine.cost.totalAmount.amount,
+        "currency": event.data.cartLine.cost.totalAmount.currencyCode,
+        "transaction_id": ""
+    });
+    console.log(dataLayer);
+});
+
+// GA4 begin_checkout
+analytics.subscribe("checkout_started", (event) => {
+    gtag("event", "conversion", {
+        "send_to": "AW-XXXXXXXX/YYYYYYYYYYYYY",
+        "value": event.data.checkout.totalPrice.amount,
+        "currency": event.data.checkout.currencyCode,
+        "transaction_id": ""
+    });
+    console.log(dataLayer);
+});
+
+// GA4 purchase
+analytics.subscribe("checkout_completed", (event) => {
+    gtag("set", "user_data", {
+        "email": event.data.checkout.email
+    });
+    gtag("event", "conversion", {
+        "send_to": "AW-XXXXXXXX/YYYYYYYYYYYYY",
+        "value": event.data.checkout.totalPrice.amount,
+        "currency": event.data.checkout.currencyCode,
+        "transaction_id": event.data.checkout.order.id
+    });
+    console.log(dataLayer);
+});
---
+++ meta-custom-pixel.js
@@ -0,0 +1,49 @@
+!function (f, b, e, v, n, t, s) {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+        n.callMethod ?
+            n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = "2.0";
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s)
+}(window, document, "script",
+    "https://connect.facebook.net/en_US/fbevents.js");
+fbq("init", 123456789);
+
+// integrate third-party pixel tracking
+analytics.subscribe("page_viewed", (event) => {
+    fbq("track", "PageView");
+});
+
+analytics.subscribe("product_viewed", (event) => {
+    fbq("track", "ViewContent", {
+        currency: event.data.productVariant.price.currencyCode,
+        value: event.data.productVariant.price.amount,
+    });
+});
+
+analytics.subscribe("product_added_to_cart", (event) => {
+    fbq("track", "AddToCart", {
+        currency: event.data.cartLine.merchandise.productVariant.price.currencyCode,
+        value: event.data.cartLine.merchandise.productVariant.price.amount,
+    });
+});
+
+analytics.subscribe("checkout_started", (event) => {
+    fbq("track", "InitiateCheckout");
+});
+
+analytics.subscribe("checkout_completed", (event) => {
+    fbq("track", "Purchase", {
+        currency: event.data.checkout.currencyCode,
+        value: event.data.checkout.totalPrice.amount,
+    });
+});
