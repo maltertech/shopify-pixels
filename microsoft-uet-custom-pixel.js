8097ea498c5053dbef32f5359a6f2379a93353a4
zach <zach@maltertech.com>
2025-05-19 15:20:44 -0400

    bing

--- microsoft-uet-custom-pixel.js
+++ microsoft-uet-custom-pixel.js
@@ -1,7 +1,7 @@
 (function (w, d, t, r, u) {
     var f, n, i;
     w[u] = w[u] || [], f = function () {
-        var o = {ti: "XXXXXXXXXXXXXXXXXXX", enableAutoSpaTracking: true};
+        var o = {ti: "XXXXXXXXXX", enableAutoSpaTracking: true};
         o.q = w[u], w[u] = new UET(o), w[u].push("pageLoad")
     }, n = d.createElement(t), n.src = r, n.async = 1, n.onload = n.onreadystatechange = function () {
         var s = this.readyState;
@@ -10,12 +10,26 @@
 })(window, document, "script", "//bat.bing.com/bat.js", "uetq");
 
 
-analytics.subscribe("checkout_completed", (event) => {
-    uetq = uetq || [];
-    uetq.push('event', 'purchase',
-        {
-            'revenue_value': event.data.checkout.totalPrice.amount,
-            'currency': event.data.checkout.currencyCode
-        });
+// add to cart
+analytics.subscribe("product_added_to_cart", (event) => {
+    window.uetq = window.uetq || [];
+    window.uetq.push("event", "add_to_cart", {
+        "revenue_value": event.data.cartLine.cost.totalAmount.amount,
+        "currency": event.data.cartLine.cost.totalAmount.currencyCode,
+    });
+});
 
+// checkout completed
+analytics.subscribe("checkout_completed", (event) => {
+    window.uetq = window.uetq || [];
+    window.uetq.push('set', {
+        "pid": {
+            "em": event.data.checkout.email,
+            "ph": event.data.checkout.phone
+        }
+    });
+    window.uetq.push("event", "purchase", {
+        "revenue_value": Replevent.data.checkout.totalPrice.amount,
+        "currency": event.data.checkout.currencyCode
+    });
 });
